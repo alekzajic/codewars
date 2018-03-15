5dfe26a11410dcac6b0aa34a16bdfad9b971180e
Aleksandar Zajic <alisteoroz@gmail.com>
2018-03-15 17:06:13 +0100

    added tests

--- 4kyu/pyramid_slide_down/pyramid_slide_down.js
+++ 4kyu/pyramid_slide_down/pyramid_slide_down.js
@@ -6,12 +6,15 @@ longestSlideDown = (pyramid) => {
         return (index === pyramid.length - 1) ? elem : 0;
       }));
     });
+    console.log("ssd",pyramidSum);
   
     for (let i = pyramidSum.length - 2; i >= 0; i--) {
         for (let j = 0; j < pyramidSum[i].length; j++) {
             pyramidSum[i][j] = pyramid[i][j] + Math.max(pyramidSum[i+1][j], pyramidSum[i+1][j+1]);
         }
     }
+
+    console.log("ssd2",pyramidSum);
     return pyramidSum[0][0];
 }
 
--- 6kyu/transpose_matrix/transpose_matrix.js
+++ 6kyu/transpose_matrix/transpose_matrix.js
@@ -10,5 +10,20 @@ const transpose = (matrix) => {
     return m;
   }
 
+// TEST
+
 console.log(transpose([[1]])); //, [[1]]);
 console.log(transpose([[1,2,3],[4,5,6]])); //, [[1,4],[2,5],[3,6]]);
+
+// BEST
+
+function transpose2(matrix) {
+  return matrix[0].map(function(uselessValue, colIndex){
+      return matrix.map(function(uselessRow, rowIndex){
+          return matrix[rowIndex][colIndex];
+      });
+  });
+}
+
+console.log(transpose2([[1]])); //, [[1]]);
+console.log(transpose2([[1,2,3],[4,5,6]])); //, [[1,4],[2,5],[3,6]]);
